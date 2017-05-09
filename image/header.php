4d96f056d5243c5e424926757fae455e15bf2084
Paretkf <paretk7@gmail.com>
2017-05-09 22:49:42 +0700

    header

---
+++ image/header.php
@@ -0,0 +1,30 @@
+<?php include 'connect.php'; ?>
+<!-- Nav bar-->
+  <nav class="nav has-shadow">
+<div class="container">
+  <div class="nav-left">
+    <a class="nav-item is-tab is-hidden-mobile is-active">SSNC-Civil</a>
+    <a class="nav-item is-tab is-hidden-mobile">ข้อมูลพนักงาน</a>
+    <a class="nav-item is-tab is-hidden-mobile">เพิ่มข้อมูลพนักงาน</a>
+    <a class="nav-item is-tab is-hidden-mobile">About</a>
+  </div>
+  <span class="nav-toggle">
+    <span></span>
+    <span></span>
+    <span></span>
+  </span>
+  <div class="nav-right nav-menu">
+    <a class="nav-item is-tab is-hidden-tablet is-active">Home</a>
+    <a class="nav-item is-tab is-hidden-tablet">Features</a>
+    <a class="nav-item is-tab is-hidden-tablet">Pricing</a>
+    <a class="nav-item is-tab is-hidden-tablet">About</a>
+    <a class="nav-item is-tab">
+      <figure class="image is-16x16" style="margin-right: 8px;">
+
+      </figure>
+      Profile
+    </a>
+    <a class="nav-item is-tab">Log out</a>
+  </div>
+</div>
+</nav>
