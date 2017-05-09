b4b84c9ed27c3f5ea3c3c697b76dfb6d81b5a0ed
HashtagF <flamezaxaou1@gmail.com>
2017-05-10 00:15:32 +0700

    สุดท้ายข้องวันนี้ 09/05/60

--- template/footer.php
+++ template/footer.php
@@ -1,8 +1,7 @@
-<footer class="footer" style="background-color: #eeeeee;height : 80px">
+<footer  style="background-color: #CCCCCC;height : 140px;" class="has-shadow">
   <div class="container" >
     <div class="content has-text-centered">
-        <p><h1 style="color : #00d1b2">SSNC-CIVIL.COM</h1></p>
+        <p><h1 style="color : #00d1b2;"><br>SSNC-CIVIL.COM</h1></p>
     </div>
   </div>
-
 </footer>
