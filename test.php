3b36797d5eae13e0c6b95955a52e9185e279413b
HashtagF <flamezaxaou1@gmail.com>
2017-05-12 00:41:37 +0700

    ขึ้นหน้าใหม่

--- test.php
+++ test.php
@@ -1,1 +1,40 @@
-<a href="https://giphy.com/search/taeyeon"></a>
+
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function printContent(el){
+    var restorepage = document.body.innerHTML;
+    var printcontent = document.getElementById(el).innerHTML;
+    document.body.innerHTML = printcontent;
+    window.print();
+    document.body.innerHTML = restorepage;
+
+}
+</script>
+</head>
+<body>
+<h1>My page</h1>
+<div id="div1">DIV 1 content...</div>
+<button onclick="printContent('div1')">Print Content</button>
+<div id="div2">DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br> ขึ้นหน้าใหม่ <div style="page-break-after: always"></div>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+  DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>DIV 2 content... <br>
+
+</div>
+<button onclick="printContent('div2')">Print Content</button>
+<p id="p1">Paragraph 1 content...</p>
+<button onclick="printContent('p1')">Print Content</button>
+
+</body>
+</html>
