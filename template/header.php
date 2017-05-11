8ade4b68fea24146dde6a6c8e7248b36f93e8fd9
miw1513 <miw1513@hotmail.com>
2017-05-11 18:01:32 +0700

    Updated show_salary.php
    Updated show_salary.php

--- template/header.php
+++ template/header.php
@@ -6,9 +6,17 @@
     <title>SSNC-Civil</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.1/css/bulma.css" />
     <link rel="stylesheet" href="css/style.css">
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <!-- Compiled and minified JavaScript -->
+    <script type="text/javascript">
+    function toggleDiv(divId){
+        $("#"+divId).toggle();
+        $("#1"+divId).toggle();
+        $("#2"+divId).toggle();
+    }
+
+    </script>
 
 
   </head>
