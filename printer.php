2b81ab2ecf551baeec7e6357f8c361f124a00c90
miw1513 <miw1513@hotmail.com>
2017-05-12 14:56:32 +0700

    updated
    updated

--- printer.php
+++ printer.php
@@ -14,68 +14,45 @@ require 'template/header.php';
       }
     </script>
 <div class="container">
-    <div id="div1" class="column">
-        <div style="width:550px; height:820px;">
-           asdasdasdasdasdasdasd<br>
-            asdasdasdasdasdasdasd<br>
-             asdasdasdasdasdasdasd<br>
-              asdasdasdasdasdasdasd<br>
-               asdasdasdasdasdasdasd<br>
-                asdasdasdasdasdasdasd<br>
-                 asdasdasdasdasdasdasd<br>
-                  asdasdasdasdasdasdasd<br>
-                   asdasdasdasdasdasdasd<br>
-                    asdasdasdasdasdasdasd<br>
-                     asdasdasdasdasdasdasd<br>
-                      asdasdasdasdasdasdasd<br>
-                       asdasdasdasdasdasdasd<br>3
-                        asdasdasdasdasdasdasd<br>
-                         asdasdasdasdasdasdasd<br>
-                          asdasdasdasdasdasdasd<br>
-                           asdasdasdasdasdasdasd<br>
-                            asdasdasdasdasdasdasd<br>
-                             asdasdasdasdasdasdasd<br>
-                              asdasdasdasdasdasdasd<br>
-                               asdasdasdasdasdasdasd<br>
-                                asdasdasdasdasdasdasd<br>
-                                 asdasdasdasdasdasdasd<br>
-                                  asdasdasdasdasdasdasd<br>
-                                   asdasdasdasdasdasdasd<br>
-                                    asdasdasdasdasdasdasd<br>
-                                     asdasdasdasdasdasdasd<br>
-                                      asdasdasdasdasdasdasd<br>
-                                       asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                         asdasdasdasdasdasdasd<br>
-                                        s  asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
-                                        asdasdasdasdasdasdasd<br>
 
-
-
-
-
-
-     </div>
-
-
-
-   </div>
-    <button type="button" onclick="printContent('div1')">sdsd</button>
 
 
   <?php
     if(isset($_GET['printer'])){
     $printer = $_GET['printer'];
 
+    ?>
+    <div id="div1" class="column">
+      <?php
 
+      for ($i=0;$i<count($printer);$i = $i+2){
+
+
+        ?>
+         <div style="width:550px; height:820px;">
+        <?php
+        for ($u=0;$u<2;$u++){
+          $sql_select_print = $sqlselect = "SELECT * FROM salary INNER JOIN employees ON salary.Salary_ID = employees.Emp_ID INNER JOIN time ON salary.Salary_ID=time.Time_ID WHERE Emp_ID = '".$printer[$i+$u]."' ";
+          $query_sql_select = $connect->query($sql_select_print);
+          $select_print = $query_sql_select->fetch_assoc();
+
+          echo $select_print['Emp_ID']."eiei <br>";
+        ?>
+
+
+
+ <?php
+    }
+    ?>
+    <div style="page-break-after: always"></div>
+    </div>
+    <?php
+   }
+   ?>
+
+    <button type="button" onclick="printContent('div1')">sdsd</button>
+
+    <?php
     }
 
     else {
